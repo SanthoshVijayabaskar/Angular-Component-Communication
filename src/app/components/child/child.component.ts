7b6c140be46363d1e7ace9040225680061dd8e21
Santhosh Vijay <santhoshthepro@gmail.com>
2019-02-22 23:18:44 -0500

    Parent to Child via Input Decorator

---
+++ src/app/components/child/child.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Input() childMessage: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
